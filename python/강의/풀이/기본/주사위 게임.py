969f1d896678ca17cee17240ebe945a136f4df32
donggyu <dongury1114@gmail.com>
2022-06-13 11:03:22 +0900

    2022.06.13

--- python/강의/풀이/기본/주사위 게임.py
+++ python/강의/풀이/기본/주사위 게임.py
@@ -1,17 +1,26 @@
 import sys
 from collections import Counter
 sys.stdin = open("input.txt")
-
+# 출력 16000
 n = int(input())
-num = list(list(map(int, input().split()))for _ in range(n))
-tmp = []
 
-for i in num:
-    a = list(map(list, Counter(i).most_common()))
-    if a[0][1] == 3:
-        tmp.append(10000 + a[0][0] * 1000)
-    elif a[0][1] == 2:
-        tmp.append(1000 + a[0][0] * 100)
-    else:
-        tmp.append(max(a[0]) * 100)
-print(max(tmp))
+for i in range(n):
+    tmp = input().split()
+    tmp.sort()
+    print(tmp)
+
+
+# sol.01
+# n = int(input())
+# num = list(list(map(int, input().split()))for _ in range(n))
+# tmp = []
+
+# for i in num:
+#     a = list(map(list, Counter(i).most_common()))
+#     if a[0][1] == 3:
+#         tmp.append(10000 + a[0][0] * 1000)
+#     elif a[0][1] == 2:
+#         tmp.append(1000 + a[0][0] * 100)
+#     else:
+#         tmp.append(max(a[0]) * 100)
+# print(max(tmp))
