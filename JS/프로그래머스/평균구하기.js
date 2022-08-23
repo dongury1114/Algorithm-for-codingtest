0a3ff960e73be9d15fe4f85066190ca1ba115780
donggyu <dongury1114@gmail.com>
2022-08-23 11:47:31 +0900

    2022.08.23

---
+++ JS/프로그래머스/평균구하기.js
@@ -0,0 +1,13 @@
+function solution(arr) {
+    var answer = 0;
+    let sum = 0;
+    
+    for(let i = 0; i<arr.length ;i++){
+        sum += arr[i];
+    }
+    return answer = sum/arr.length;
+}
+
+function solution(arr) {
+    return arr.reduce((a,b) => a+b) / arr.length;
+}
