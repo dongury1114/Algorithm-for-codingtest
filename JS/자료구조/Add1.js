cdd521ac1aa3db1907a53e8dcb4bc0226e3dec91
donggyu <dongury1114@gmail.com>
2022-09-05 13:04:58 +0900

    2022.09.05

--- JS/자료구조/Add1.js
+++ JS/자료구조/Add1.js
@@ -10,4 +10,4 @@ function addUpTo(n) {
 let t1 = performance.now();
 addUpTo(100000000);
 let t2 = performance.now();
-console.log(`Time Elapsed: ${(t2-t1)/1000} seconds.`);
+console.log(`Time Elapsed: ${(t2 - t1) / 1000} seconds.`);
