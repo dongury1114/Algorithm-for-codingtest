f13989285a525568ddae06255255444080daf139
donggyu <dongury1114@gmail.com>
2022-10-02 22:36:29 +0900

    2022.10.02

--- JS/인프런/myCode/완전탐색(브루투포스)/2. 뒤집은 소수.js
+++ JS/인프런/myCode/완전탐색(브루투포스)/2. 뒤집은 소수.js
@@ -5,19 +5,29 @@ function isPrime(num) {
     }
     return true
 }
+// function solution(arr) {
+//     let answer = [];
+
+//     for (let x of arr) {
+//         let res = 0;
+//         while (x) {
+//             let t = x % 10;
+//             res = res * 10 + t;
+//             x = parseInt(x / 10)
+//         }
+//         if (isPrime(res)) answer.push(res)
+//     }
+
+//     return answer
+// }
+
 function solution(arr) {
     let answer = [];
 
     for (let x of arr) {
-        let res = 0;
-        while (x) {
-            let t = x % 10;
-            res = res * 10 + t;
-            x = parseInt(x / 10)
-        }
+        let res = parseInt(x.toString().split('').reverse().join(''));
         if (isPrime(res)) answer.push(res)
     }
-
     return answer
 }
 
