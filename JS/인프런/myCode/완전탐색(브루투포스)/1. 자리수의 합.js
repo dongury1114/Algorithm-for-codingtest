6e90abe4307e0c266c9fdb8531a65decbe5b054d
donggyu <dongury1114@gmail.com>
2022-10-02 22:10:03 +0900

    2022.10.02

--- JS/인프런/myCode/완전탐색(브루투포스)/1. 자리수의 합.js
+++ JS/인프런/myCode/완전탐색(브루투포스)/1. 자리수의 합.js
@@ -1,12 +1,27 @@
+// function solution(arr) {
+//     let answer, max = Number.MIN_SAFE_INTEGER;
+
+//     for (let x of arr) {
+//         let sum = 0, tmp = x;
+//         while (tmp) {
+//             sum += (tmp % 10)
+//             tmp = Math.floor(tmp / 10);
+//         }
+//         if (sum > max) {
+//             max = sum;
+//             answer = x;
+//         } else if (sum === max) {
+//             if (x > answer) answer = x
+//         }
+//     }
+//     return answer
+// }
+
 function solution(arr) {
     let answer, max = Number.MIN_SAFE_INTEGER;
 
     for (let x of arr) {
-        let sum = 0, tmp = x;
-        while (tmp) {
-            sum += (tmp % 10)
-            tmp = Math.floor(tmp / 10);
-        }
+    let sum = x.toString().split("").reduce((a,b) => a+ Number(b), 0)
         if (sum > max) {
             max = sum;
             answer = x;
@@ -16,6 +31,5 @@ function solution(arr) {
     }
     return answer
 }
-
 let input = [128, 460, 603, 40, 521, 137, 123]
 console.log(solution(input));
